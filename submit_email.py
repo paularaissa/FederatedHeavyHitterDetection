eb4a0f2ebc64c5ed1bbf0a0ad4dd4b9882e7f6ad
Paula Raissa <paula.csraissa@gmail.com>
2026-03-06 16:58:41 +0000

    Remove sensitive information from email script
    Removed hardcoded email credentials and subject.

--- submit_email.py
+++ submit_email.py
@@ -2,13 +2,13 @@ import yagmail
 
 def send_email(filename):
     # Configurações do Gmail
-    sender_email = 'paula.csraissa@gmail.com'
-    sender_password = 'smsf xixj nkia zrtb'
+    sender_email = ''
+    sender_password = ''
 
     # Crie o objeto de e-mail
-    subject = 'Server Inesctec'
-    contents = 'Olá, seu codigo terminou de executar ' + filename 
-    receiver_email = 'paula.csraissa@gmail.com'
+    subject = ''
+    contents = '' + filename 
+    receiver_email = ''
 
     # Envie o e-mail
     yag = yagmail.SMTP(sender_email, sender_password)
